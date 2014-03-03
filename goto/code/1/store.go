cb9a246c3370b10fcaf8d48f995c7764a23fd5ca
kedebug <sunweiqq@gmail.com>
2014-03-03 11:06:41 +0800

    update to v2.0

---
+++ goto/code/1/store.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"sync"
+)
+
+type URLStore struct {
+	urls map[string]string
+	mu   sync.RWMutex
+	file *os.File
+}
+
+type record struct {
+	key, url string
+}
+
+func NewURLStore(filename string) *URLStore {
+	s := &URLStore{urls: make(map[string]string)}
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal("URLStore: ", err)
+	}
+	s.file = f
+	if err := s.load(); err != nil {
+		log.Println("URLStore: ", err)
+	}
+	return s
+}
+
+func (s *URLStore) Get(key string) string {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.urls[key]
+}
+
+func (s *URLStore) Set(key, url string) bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	_, present := s.urls[key]
+	if present {
+		return false
+	}
+	s.urls[key] = url
+	return true
+}
+
+func (s *URLStore) Count() int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return len(s.urls)
+}
+
+func (s *URLStore) Put(url string) string {
+	for {
+		key := genKey(s.Count())
+		if s.Set(key, url) {
+			if err := s.save(key, url); err != nil {
+				log.Println("URLStore: ", err)
+			}
+			return key
+		}
+	}
+	panic("shouldn't get here")
+}
+
+func (s *URLStore) load() error {
+	if _, err := s.file.Seek(0, 0); err != nil {
+		return err
+	}
+	d := json.NewDecoder(s.file)
+	var err error
+	for err == nil {
+		var r record
+		if err = d.Decode(&r); err == nil {
+			s.Set(r.key, r.url)
+		}
+	}
+	if err == io.EOF {
+		return nil
+	}
+	return err
+}
+
+func (s *URLStore) save(key, url string) error {
+	e := json.NewEncoder(s.file)
+	return e.Encode(record{key, url})
+}
