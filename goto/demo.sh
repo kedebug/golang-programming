04f191fb0db8451a3500e06390368703dfd75078
kedebug <sunweiqq@gmail.com>
2014-03-04 15:06:22 +0800

    update files

---
+++ goto/demo.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+STATS=127.0.0.1:8090
+MASTER=127.0.0.1:8080
+N1=1
+N2=8
+N3=16
+
+echo "Starting up"
+cd ../stat/server
+go build -o stats
+./stats &
+stats_pid=$!
+cd ../../goto
+sleep 1
+go build -o goto
+./goto -stats=$STATS -host=$MASTER -rpc=true &
+master_pid=$!
+sleep 1
+./goto -stats=$STATS -host=$MASTER -master=$MASTER -http=:8081 &
+slave1_pid=$!
+./goto -stats=$STATS -host=$MASTER -master=$MASTER -http=:8082 &
+slave2_pid=$!
+./goto -stats=$STATS -host=$MASTER -master=$MASTER -http=:8083 &
+slave3_pid=$!
+sleep 1
+
+echo "Testing the master (n=$N1)"
+go build -o bench/bench ./bench
+bench/bench -stats=$STATS -host=$MASTER -n=$N1 &
+pid=$!
+read 1
+kill $pid
+
+echo "Testing the master (n=$N2)"
+bench/bench -stats=$STATS -host=$MASTER -n=$N2 &
+pid=$!
+read 1
+kill $pid
+
+echo "Testing 1 slave (n=$N2)"
+bench/bench -stats=$STATS -host=127.0.0.1:8081 -n=$N2 &
+pid=$!
+read 1
+kill $pid
+
+echo "Testing 2 slaves (n=$N2)"
+bench/bench -stats=$STATS -host=127.0.0.1:8081,127.0.0.1:8082 -n=$N2 &
+pid=$!
+read 1
+kill $pid
+
+echo "Testing 3 slaves (n=$N2)"
+bench/bench -stats=$STATS -host=127.0.0.1:8081,127.0.0.1:8082,127.0.0.1:8083 -n=$N2 &
+pid=$!
+read 1
+kill $pid
+
+echo "Testing 3 slaves (n=$N3)"
+bench/bench -stats=$STATS -host=127.0.0.1:8081,127.0.0.1:8082,127.0.0.1:8083 -n=$N3 &
+pid=$!
+read 1
+kill $pid
+
+echo "Shutting down"
+kill $stats_pid
+kill $master_pid
+kill $slave1_pid
+kill $slave2_pid
+kill $slave3_pid
---
+++ goto/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/nf/stat"
+	"net/http"
+	"net/rpc"
+)
+
+var (
+	listenAddr = flag.String("http", ":8080", "http listen address")
+	dataFile   = flag.String("file", "store.json", "data store file name")
+	hostname   = flag.String("host", "localhost:8080", "http host name")
+	masterAddr = flag.String("master", "", "RPC master address")
+	rpcEnabled = flag.Bool("rpc", false, "enable rpc server")
+	statServer = flag.String("stats", "", "stat server address")
+)
+
+var store Store
+
+func main() {
+	flag.Parse()
+	if *masterAddr != "" {
+		store = NewProxyStore(*masterAddr)
+	} else {
+		store = NewURLStore(*dataFile)
+	}
+	if *rpcEnabled {
+		rpc.RegisterName("Store", store)
+		rpc.HandleHTTP()
+	}
+	if *statServer != "" {
+		stat.Process = *listenAddr
+		go stat.Monitor(*statServer)
+	}
+	http.HandleFunc("/", Redirect)
+	http.HandleFunc("/add", Add)
+	http.ListenAndServe(*listenAddr, nil)
+}
+
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Path[1:]
+	if key == "" {
+		http.NotFound(w, r)
+		return
+	}
+	var url string
+	if err := store.Get(&key, &url); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, url, http.StatusFound)
+}
+
+func Add(w http.ResponseWriter, r *http.Request) {
+	url := r.FormValue("url")
+	if url == "" {
+		fmt.Fprint(w, AddForm)
+		return
+	}
+	var key string
+	if err := store.Put(&url, &key); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "http://%s/%s", *hostname, key)
+}
+
+const AddForm = `
+<html>
+<form method="POST" action="/add">
+URL: <input type="text" name="url">
+<input type="submit" value="Add">
+</form>
+</html>
+`
